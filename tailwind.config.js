d2fa206338ec46e43e8198eb94dd2b4bbe9e9778
just-osaze <osaigbevo.osaz@gmail.com>
2023-09-17 23:06:19 +0100

    Added color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: { 
+        blue: '#4B5EFC',
+        darkBlue: '#17171C',
+        ash: {
+          100: '#4E505F',
+          200: '#2E2F38',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
