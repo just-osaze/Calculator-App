1f5cfcd0df64dd0f30e7891b6df61516912e5c15
just-osaze <osaigbevo.osaz@gmail.com>
2023-09-25 13:53:54 +0100

    Added handleEqualButton function, handlePercentageButton & BackSpace icon from mui

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,87 +1,162 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import BackspaceOutlinedIcon from '@mui/icons-material/BackspaceOutlined';
 
 const Calculator: React.FC = () => {
-  const [input, setInput] = useState<string>('2');
+ 
+
+
+
+  const [input, setInput] = useState<string>('');
   const [result, setResult] = useState<number | null>(null);
 
-  //Event Handler for number buttons
   const handleNumberButton = (number: string) => {
-    //update the input to append the clicked number
-    setInput((prevInput) => prevInput + number)
-  }
-  //Event Handler for clear Button (C)
+    setInput((prevInput) => prevInput + number);
+  };
+
   const handleClearButton = () => {
-    //clear the input screen and set result screen to 0
     setInput('');
     setResult(null);
-  }
+  };
+
   const handleOperatorButton = (operator: string) => {
-    //check if an operator exist
+    // Check if an operator exists
     const lastChar = input.charAt(input.length - 1);
-    if (lastChar === '+' || lastChar === '-' || lastChar === '÷' || lastChar === 'x') {
-      //if an operator exist, delete it and add the new one
-      setInput(input.slice(0, -1) + operator)
-    }else {
-      //else add the operator to the input
-      setInput(input + operator)
+    if (lastChar === '+' || lastChar === '-' || lastChar === 'x' || lastChar === '/') {
+      // If an operator exists, delete it and add the new one
+      setInput(input.slice(0, -1) + operator);
+    } else {
+      // Otherwise, add the operator to the input
+      setInput(input + operator);
+    }
+  };
+
+  const deleteLastCharacter = () => {
+    setInput(input.slice(0, -1));
+  };
+
+  const handleEqualButton = () => {
+    try {
+      // Split the input string by operators
+      const inputArray = input.split(/([+\-\x÷])/);
+
+      // Initialize variables for result and current operator
+      let result = parseFloat(inputArray[0]);
+      let currentOperator = '';
+
+      // Iterate through the input array
+      for (const item of inputArray.slice(1)) {
+        if (['+', '-', 'x', '÷'].includes(item)) {
+          // Update the operator if the item is an operator
+          currentOperator = item;
+        } else {
+          // If the item is a number, parse it as a float
+          const myNumber = parseFloat(item);
+
+          // Carry out operation based on the current operator
+          switch (currentOperator) {
+            case '+':
+              result += myNumber;
+              break;
+            case '-':
+              result -= myNumber;
+              break;
+            case 'x':
+              result *= myNumber;
+              break;
+            case '÷':
+              if (myNumber !== 0) {
+                result /= myNumber;
+              } else {
+                setResult(null);
+                setInput('');
+                return;
+              }
+              break;
+            default:
+              // Handle invalid operator error
+              setResult(null);
+              setInput('');
+              return;
+          }
+        }
+      }
+
+      // Update the result state with the final calculated result
+      setResult(result);
+    } catch (error) {
+      // Handle errors in the input expression
+      setResult(null);
+      setInput('');
+    }
+  };
+
+  const handlePercentageButton = (operator: string) => {
+    try {
+      const percentNumber = parseFloat(input);
+
+      const result = percentNumber / 100;
+
+      //update the result state
+      setResult(result);
+    } catch (error) {
+      setResult(null)
+      setInput('');
     }
   }
-  //Handle Event dor deleting last character
-  const deleteLastCharacter = () => {
-    //delete the previous number or operation
-    setInput(input.slice(0, -1));
-  }
-
 
 
   return (
-    <div className='h-screen bg-darkBlue text-white w-[375px]'>
-      <div className='flex justify-end'>
-      <input className='mr-8 mt-8 text-4xl text-end bg-darkBlue text-white' type='text' value={input} />
-      </div>
-      
-      <div className='mr-8 text-7xl text-end'>
-        {(result !== null ? result : '0')}
-      </div>
-
-      <div className='grid'>
-        <div>
-          <button className='bg-ash-100' onClick={() => handleClearButton()}>C</button>
-          <button className='bg-ash-100' onClick={() => handleOperatorButton('±')}>±</button>
-          <button className='bg-ash-100' onClick={() => handleNumberButton('%')}>%</button>
-          <button className='bg-blue' onClick={() => handleOperatorButton('÷')}>÷</button>
+    <div className='h-screen bg-darkBlue text-white m-auto grid place-items-center'>
+      <div className='-mt-32 w-[375px]'>
+        <div className=' flex'>
+          <input className='mt-8 text-4xl bg-darkBlue text-white' type='text' value={input} />
         </div>
         
-        <div>
-          <button className='bg-ash-200' onClick={() => handleNumberButton('7')}>7</button>
-          <button className='bg-ash-200' onClick={() => handleNumberButton('8')}>8</button>
-          <button className='bg-ash-200' onClick={() => handleNumberButton('9')}>9</button>
-          <button className='bg-blue' onClick={() => handleOperatorButton('x')}>x</button>
-        </div>
-        <div>
-          <button className='bg-ash-200' onClick={() => handleNumberButton('4')}>4</button>
-          <button className='bg-ash-200' onClick={() => handleNumberButton('5')}>5</button>
-          <button className='bg-ash-200' onClick={() => handleNumberButton('6')}>6</button>
-          <button className='bg-blue' onClick={() => handleOperatorButton('-')}>-</button>
-        </div>
-        
-        <div>
-          <button className='bg-ash-200' onClick={() => handleNumberButton('1')}>1</button>
-          <button className='bg-ash-200' onClick={() => handleNumberButton('2')}>2</button>
-          <button className='bg-ash-200' onClick={() => handleNumberButton('3')}>3</button>
-          <button className='bg-blue' onClick={() => handleOperatorButton('+')}>+</button>
+        <div className='mt-4 text-7xl'>
+          {(result !== null ? result : '0')}
         </div>
 
-        <div>
-          <button className='bg-ash-200' onClick={() => handleNumberButton('.')}>.</button>
-          <button className='bg-ash-200' onClick={() => handleNumberButton('0')}>0</button>
-          <button className='bg-ash-200' onClick={() => deleteLastCharacter()}>3</button>
-          <button className='bg-blue' onClick={() => handleOperatorButton('=')}>=</button>
+        <div className='mt-4'>
+          <div>
+            <button className='bg-ash-100' onClick={() => handleClearButton()}>C</button>
+            <button className='bg-ash-100' onClick={() => handleOperatorButton('±')}>±</button>
+            <button className='bg-ash-100' onClick={() => handlePercentageButton('%')}>%</button>
+            <button className='bg-blue' onClick={() => handleOperatorButton('÷')}>÷</button>
+          </div>
+          
+          <div>
+            <button className='bg-ash-200' onClick={() => handleNumberButton('7')}>7</button>
+            <button className='bg-ash-200' onClick={() => handleNumberButton('8')}>8</button>
+            <button className='bg-ash-200' onClick={() => handleNumberButton('9')}>9</button>
+            <button className='bg-blue' onClick={() => handleOperatorButton('x')}>x</button>
+          </div>
+          <div>
+            <button className='bg-ash-200' onClick={() => handleNumberButton('4')}>4</button>
+            <button className='bg-ash-200' onClick={() => handleNumberButton('5')}>5</button>
+            <button className='bg-ash-200' onClick={() => handleNumberButton('6')}>6</button>
+            <button className='bg-blue' onClick={() => handleOperatorButton('-')}>-</button>
+          </div>
+          
+          <div>
+            <button className='bg-ash-200' onClick={() => handleNumberButton('1')}>1</button>
+            <button className='bg-ash-200' onClick={() => handleNumberButton('2')}>2</button>
+            <button className='bg-ash-200' onClick={() => handleNumberButton('3')}>3</button>
+            <button className='bg-blue' onClick={() => handleOperatorButton('+')}>+</button>
+          </div>
+
+          <div>
+            <button className='bg-ash-200' onClick={() => handleNumberButton('.')}>.</button>
+            <button className='bg-ash-200' onClick={() => handleNumberButton('0')}>0</button>
+            <button className='bg-ash-200' onClick={() => deleteLastCharacter()}>
+              <BackspaceOutlinedIcon className='flex -mt-3 align-middle'/>
+            </button>
+            <button className='bg-blue' onClick={() => handleEqualButton()}>=</button>
+          </div>
+          
         </div>
-        
       </div>
     </div>
+      
   )
 }
 
